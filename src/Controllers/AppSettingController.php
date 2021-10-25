635065b21e9ddc7aef3f89013b1da5dbb32a7dfb
turkialawlqy <turkialawlqy@gmail.com>
2021-10-25 14:53:13 +0300

    first commit

---
+++ src/Controllers/AppSettingController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Turki\AppSettings\Controllers;
+
+use Illuminate\Routing\Controller;
+use Turki\AppSettings\SavesSettings;
+
+class AppSettingController extends Controller
+{
+    use SavesSettings;
+}
